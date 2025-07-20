c13814b14d621535a202d0c4dd579aada0a4ec1c
arisd <arisdunn@gmail.com>
2025-07-19 19:37:19 -0500

    Checked letter inputs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //unordered list for guessed letters
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersUl = document.querySelector(".guessed-letters");
 //Guess button
 const guess = document.querySelector(".guess");
 //text input for guessing letter
@@ -9,13 +9,14 @@ const progress = document.querySelector(".word-in-progress");
 //paragraph to show remaining guesses
 const remaining = document.querySelector(".remaining");
 //span for remaining guesses
-const numRemaining = document.querySelector("span");
+const numRemaining = document.querySelector(".remaining span");
 //paragraph for messages to player
 const message = document.querySelector(".message");
 //play again button
 const playAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 //display placeholders
 const placeholders = function (word) {
@@ -31,7 +32,39 @@ placeholders(word);
 
 guess.addEventListener("click", function(e) {
     e.preventDefault();
-    const choose = pickLetter;
-    console.log(choose);
-    choose.value = "";
+    message.innerText = "";
+
+    const choose = pickLetter.value;
+    console.log(choose); 
+    choose.value = "";  
+
+    const goodGuess = checkInput(choose); 
+    if (goodGuess) {
+      makeGuess(choose);
+    }  
+    pickLetter.value = "";
 });
+
+//check input
+const checkInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Please enter a letter.";
+    } else if (input.length > 1) {
+        message.innerText = "Only enter one letter please.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z.";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function(choose) {
+    choose = choose.toUpperCase();
+    if (guessedLetters.includes(choose)) {
+        message.innerText = "You already guessed that letter. Please try again.";
+    } else {
+        guessedLetters.push(choose);
+        console.log(guessedLetters);
+    }
+};
